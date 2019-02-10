374cf01c5cdf04c216bf0c57057a64b80798fc9c
Jon Pearse <hello@jonpearse.net>
2019-02-10 14:42:57 +0000

    Retheming the admin nav menu a little

--- app/assets/javascripts/behaviour/admin/navigation.js
+++ app/assets/javascripts/behaviour/admin/navigation.js
@@ -4,9 +4,7 @@
  *
  *********************************************************************************************************************/
 
-const icon = require( 'util/icons' ).icon;
-
-function Navigation( elRoot, options )
+function Navigation( elRoot )
 {
     let elCurrent;
 
@@ -38,52 +36,18 @@ function Navigation( elRoot, options )
         ev.preventDefault();
     }
 
-
-    /**
-     * Closes a menu: called by clicking on a close button in the subnav.
-     */
-    function closeMenu()
-    {
-        elCurrent.classList.remove( '-current' );
-        elCurrent.querySelector( 'a' ).focus();
-
-        elCurrent = null;
-    }
-
-    /**
-     * Creates additional DOM on submenus.
-     *
-     * @param {HtmlElement} elSub - the submenu we’re looking at
-     */
-    function addAdditionalDOM( elSub )
-    {
-        // 1. create the button
-        const elButton = document.createElement( 'button' );
-        elButton.classList.add( 'main-navigation__close-button' );
-        elButton.type = 'button';
-        elButton.appendChild( icon( 'double-left' ));
-        elButton.appendChild( document.createTextNode( options.menuText ));
-        elSub.appendChild( elButton );
-
-        // 2. bind to clicking
-        elButton.addEventListener( 'click', closeMenu );
-    }
-
     /**
      * Constructor logic.
      */
     (function init()
     {
-        // 1. create some additional DOM
-        elRoot.querySelectorAll( '[data-sub-nav]' ).forEach( addAdditionalDOM );
-
-        // 2. bind to clicking on top-level links\
+        // 1. bind to clicking on top-level links\
         elRoot.querySelectorAll( '[data-top-level]' ).forEach( el => el.querySelector( 'a' ).addEventListener( 'click', handleClick ));
 
-        // 3. find whatever’s current
+        // 2. find whatever’s current
         elCurrent = elRoot.querySelector( '.-current[data-top-level]' );
 
-        // 4. add some transition flags
+        // 3. add some transition flags
         elRoot.classList.add( '-transitions' );
 
     }())
