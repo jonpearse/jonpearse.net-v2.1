9a07b166a197a8899a119590a9127feff7f71c04
Jon Pearse <hello@jonpearse.net>
2019-02-28 19:48:52 +0000

    Adding basic content to admin dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,11 @@
 # Main landing controller for CMS.
 class Admin::DashboardController < Admin::BaseController
-  
+
   def show
-    
+
+    @page_title = 'Dashboard'
+    @breadcrumb = [ 'Dashboard' ]
+
   end
-  
+
 end
