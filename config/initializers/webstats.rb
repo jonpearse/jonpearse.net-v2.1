bd235aa62abef3484f13aff2db27fc3d5d47def9
Jon Pearse <hello@jonpearse.net>
2019-03-03 12:22:37 +0000

    Improvements to webstats stuff
    - breaking code out a little bit
    - adding the ability to define additional UAs to ignore
    
    Also, nuking + rebuilding the Gemfile.lock file as the version detection seems to be a bit wonky

--- config/initializers/webstats.rb
+++ config/initializers/webstats.rb
@@ -1,5 +1,11 @@
 Rails.application.config.webstats = {
 
-  ignored_ips: [ '31.132.32.40' ]
+  # any IPs we should ignore on principle
+  ignored_ips: [ '31.132.32.40' ],
 
+  # any additional UA fragments to ignore (because Browser gem doesn’t seem to be updated often + isn’t pluggable)
+  ignored_uas: [
+    'ZoominfoBot',
+    'zgrab/0.x'
+  ]
 }
