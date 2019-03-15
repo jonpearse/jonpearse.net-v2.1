c131babe887f962780238f4ca1379d7290acc43b
Jon Pearse <hello@jonpearse.net>
2019-03-15 17:15:44 +0000

    More bot detection

--- config/initializers/webstats.rb
+++ config/initializers/webstats.rb
@@ -6,6 +6,22 @@ Rails.application.config.webstats = {
   # any additional UA fragments to ignore (because Browser gem doesn’t seem to be updated often + isn’t pluggable)
   ignored_uas: [
     'ZoominfoBot',
-    'zgrab/0.x'
+    'zgrab/0.x',
+
+    # new bots + bot-like UAs, March 15th 2019
+    'Uptimebot/1.0',
+    'Tiny Tiny RSS/19.2',
+    'Nimbostratus-Bot/v1.3.2',
+    'BazQux/2.4',
+    'Go-http-client/2.0',
+    'aiohttp/3.4.4',
+    'Datanyze',
+    'BoardReader Favicon Fetcher /1.0',
+    'Blackboard Safeassign',
+    'Daum/4.1',
+    'FunWebProducts',
+    'Wappalyzer',
+    'MetaFeedly/1.0',
+    'Feedbin'
   ]
 }
