43b4796202e875e0510246fb331acdda81b7c4b8
Jon Pearse <hello@jonpearse.net>
2019-04-09 00:03:53 +0100

    Fixing aggregation bugs/oversights

--- app/models/stats/raw.rb
+++ app/models/stats/raw.rb
@@ -3,17 +3,14 @@ class Stats::Raw < ApplicationRecord
   self.table_name = 'stats_raw'
 
   # we always want to select before today
-  default_scope -> { where(
-    '( recorded_at < ? ) AND ( browser_name != ? )',
-    Date.today.at_midnight,
-    'Generic Browser'
-  ).order( 'recorded_at ASC' )}
+  default_scope -> { where( 'recorded_at < ?', Date.today.at_midnight ).order( 'recorded_at ASC' )}
 
   # aggregation scopes
-  scope :trackable, -> { select( "DISTINCT( `session_id` )" )}
-  scope :anonymous, -> { where( session_id: nil )}
+  scope :not_generic, -> { where( 'browser_name != ?', 'Generic Browser' )}
+  scope :trackable, -> { not_generic.select( "DISTINCT( `session_id` )" ).where( 'session_id IS NOT NULL' )}
+  scope :anonymous, -> { not_generic.where( session_id: nil )}
 
   # lookup scope
-  scope :by_session, -> ( session_id ){ where( session_id: session_id )}
+  scope :by_session, -> ( session_id ){ unscoped.not_generic.where( session_id: session_id )}
 
 end
